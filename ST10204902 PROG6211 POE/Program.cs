a9d77568787fff63db76ffc3477fc4444be8cdd0
NickTheAlbatross <ST10204902@vcconnect.edu.za>
2023-04-25 17:12:03 +0200

    - Added getters and setters to Recipe class - Added primitive testing to Program class to test new toString in Recipe class

--- ST10204902 PROG6211 POE/Program.cs
+++ ST10204902 PROG6211 POE/Program.cs
@@ -11,9 +11,6 @@ namespace ST10204902_PROG6211_POE
         static void Main(string[] args)
         {
 
-            //start of application
-
-
             /*
              * The user shall be able to enter the details for a single recipe:
                 a. The number of ingredients.
@@ -23,6 +20,30 @@ namespace ST10204902_PROG6211_POE
                 d. For each step: a description of what the user should do           
              */
 
+            //start of application
+            int steps = 3;
+            int ingredients = 3;
+            Recipe r = new Recipe(3, 3);
+            string[] arrSteps = { "Wash vegetables", "Cook food", "Eat food" };
+
+            for (int i = 0; i < ingredients; i++)
+            {
+                Ingredient ing = new Ingredient();
+                Console.WriteLine("Enter ingredient name:");
+                ing.setName(Console.ReadLine());
+                Console.WriteLine("Enter the quanity for this item: ");
+                ing.setQuantity(Double.Parse(Console.ReadLine()));
+                Console.WriteLine("Enter the unit of measurement for this item: ");
+                ing.setUnitOfMeasurement(Console.ReadLine());
+                r.addIngredientToArray(ing);
+            }
+            
+            Console.WriteLine(r.toString());
+
+            
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
             //end of application
         }
     }
--- ST10204902 PROG6211 POE/Recipe.cs
+++ ST10204902 PROG6211 POE/Recipe.cs
@@ -8,12 +8,56 @@
         private int numSteps;
         private Ingredient[] arrIngredients;
         private string[] arrSteps;
+        private int ingredientCount=0;
+        private int stepCount = 0;
 
-        //Incomplete constructor
+        //Parameterised constructor that accepts two integers that represent the length of the ingredient array and length of the steps array
+        //The constructor also sets the local variables of numIngredients and numSteps to the variables in the parameter
         public Recipe(int inNumIngredients, int inNumSteps)
         {
             arrIngredients = new Ingredient[inNumIngredients];
             arrSteps = new string[inNumSteps];
+            numIngredients= inNumIngredients;
+            numSteps = inNumSteps;
+        }
+
+        //default constructor to allow building during testing
+        public Recipe() 
+        {
+        }
+        //Custom adder method to streamline data input in main program
+
+        //Adder method for arrIngredient
+        //Accepts an Ingredient object, stores it in the array then increments the counter by 1
+
+        public void addIngredientToArray(Ingredient item)
+        {
+            arrIngredients[ingredientCount] = item;
+            ingredientCount++;
+        }
+        
+
+        //Getter methods
+        public int getNumIngredients() {  return numIngredients; }
+
+        public int getNumSteps() { return numSteps; }
+
+        public Ingredient[] getIngredientArr() { return arrIngredients; }
+        public string[] getStepsArr() { return arrSteps; }
+
+
+        //Setter methods
+
+        public void setStepsArr(string[] steps) { arrSteps = steps; }
+
+        public void setNumIngredients(int inNumIngredients)
+        {
+            numIngredients = inNumIngredients;
+        }
+
+        public void setNumSteps(int inNumSteps)
+        {
+            numSteps = inNumSteps; 
         }
         /*The user shall be able to enter the details for a single recipe:
 a. The number of ingredients.
@@ -22,7 +66,23 @@ tablespoon of sugar.
 c. The number of steps.
 d. For each step: a description of what the user should do*/
 
+        //Custom toString that returns a formatted list within the recipe
+        public string toString()
+        {
+            string temp = "";
+
+            for (int i  = 0; i < numIngredients; i++)
+            {
+                temp += "\n - Ingredient "+(i+1) + ": " +  arrIngredients[i].printIngredient();
+            }
+
+            for (int i = 0;i<numSteps; i++)
+            {
+                temp += "\nStep " + (i+1) + ": " + arrSteps[i];
+            }
 
 
+            return temp;
+        }
     }
 }
